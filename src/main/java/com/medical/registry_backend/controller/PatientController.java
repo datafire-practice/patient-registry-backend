5f9f852894a341aa08fdfd0493375a7c1829f5b9
Anastasiy <anastasiymasterova@gmail.com>
2025-07-19 18:22:16 +0400

    /patients -> /patient

--- src/main/java/com/medical/registry_backend/controller/PatientController.java
+++ src/main/java/com/medical/registry_backend/controller/PatientController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/patients")
+@RequestMapping("/patient")
 @RequiredArgsConstructor
 public class PatientController {
     private final PatientService patientService;
