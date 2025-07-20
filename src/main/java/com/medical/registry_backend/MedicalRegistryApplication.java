a15c3c78e8c6a64cbcf29f25683780e59d2c8393
Anastasiy <anastasiymasterova@gmail.com>
2025-07-20 15:57:54 +0400

    тест кэша мкб10

--- src/main/java/com/medical/registry_backend/MedicalRegistryApplication.java
+++ src/main/java/com/medical/registry_backend/MedicalRegistryApplication.java
@@ -3,6 +3,7 @@ package com.medical.registry_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
